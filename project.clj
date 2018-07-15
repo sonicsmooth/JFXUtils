5f0e6a86d030b6ec003ac8e247bf47b99a383418
Michael Schwager <mschwager@kymetacorp.com>
2018-07-14 22:51:29 -0700

    Remove annoying dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [de.codecentric.centerdevice/javafxsvg "1.0.0"]]
+                 ;;[de.codecentric.centerdevice/javafxsvg "1.0.0"]
+				 ]
   :plugins [[lein-exec "0.3.6"]]
 
   ;; The normal behavior is to init the toolkit, so change this to
